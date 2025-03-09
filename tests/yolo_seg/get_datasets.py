b34fea96444c5b5c7ff538293eeb2777ec689e77
XindongXU <xxd_ars@sjtu.edu.cn>
2025-03-10 02:12:32 +0800

    up0310

--- tests/yolo_seg/get_datasets.py
+++ tests/yolo_seg/get_datasets.py
@@ -1,11 +1,15 @@
-import os
 from roboflow import Roboflow
 
-dataset_version = 6
-location = r"./tests/yolo_seg/datasets/Blood-Scan-" + str(dataset_version)
+dataset_version = 1
+location = r"./tests/yolo_seg/datasets/Blue-Rawdata-1504-500" + "-" + str(dataset_version)
+# rf = Roboflow(api_key="0Tt9VAmQ4YXp42tYyNLA")
+# project = rf.workspace("bloodscan").project("blood-scan-lyzhe")
+# rf = Roboflow(api_key="C99pXEebpnE3dSAP3tuQ")
+# project = rf.workspace("bloodscancropped").project("blood-scan-cropped")
 
-rf = Roboflow(api_key="0Tt9VAmQ4YXp42tYyNLA")
-project = rf.workspace("bloodscan").project("blood-scan-lyzhe")
+rf = Roboflow(api_key="Q1tiegdnCfdGzkY3zduG")
+project = rf.workspace("bloodscancropped-atnul").project("coannotation-bloodscan")
+
 version = project.version(dataset_version)
 dataset = version.download(model_format = "yolov11",
                            location = location)
