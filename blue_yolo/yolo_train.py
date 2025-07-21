d8168760b95fd8f06983fd0e4b1c5a825998cf07
XindongXU <xxd_ars@sjtu.edu.cn>
2025-07-21 18:09:29 +0800

    train results 10

--- blue_yolo/yolo_train.py
+++ blue_yolo/yolo_train.py
@@ -2,19 +2,20 @@ from ultralytics import YOLO
 import os
 
 if __name__ == '__main__':
-    dataset_version = 6
-    project_directory = "/Users/ASUS/Documents/SJTU M2/Graduation Project/BloodScan/tests/yolo_seg"
+    dataset_version = 1
+    project_directory = "/home/xin99/BloodScan/tests/yolo_seg"
     # os.chdir(target_directory)
 
     model = YOLO(project_directory + "/weights/yolo11x-seg.pt")
     # model = YOLO("yolo11n.yaml").load("yolo11n.pt")
 
-    # dataset = project_directory + "/datasets/Blood-Scan-" + str(dataset_version) + '/data.yaml'
+    # dataset = project_directory + "/datasets/Blood-Scan-Cropped-" + str(dataset_version) + '/data.yaml'
     dataset = project_directory + "/datasets/Blue-Rawdata-1504-500-" + str(dataset_version) + '/data.yaml'
     
     results = model.train(data = dataset, 
-                          device="cuda", 
+                          # device="cuda", 
+                          device="0,1,2,3",
                           batch = 8, 
-                          epochs = 100, 
-                          imgsz = 1024, 
+                          epochs = 200, 
+                          imgsz = 1504, 
                           plots = True)
